11ce3dd23f068480c0109d0100ae1052ad563b80
Naween Fonseka <naween.2013074@iit.ac.lk>
2017-01-13 04:19:54 +0530

    Beer trivia game file changes

--- beer_trivia_q3.php
+++ beer_trivia_q3.php
@@ -1,5 +1,5 @@
 <?php
-include('pages/beer_trivia_pages/beer_trivia_q3.html');
+include('pages/beer_trivia_q3.html');
 include('pages/footer.html');
 ?>
 
--- beer_trivia_q5.php
+++ beer_trivia_q5.php
@@ -1,5 +1,5 @@
 <?php
-include('pages/beer_trivia_pages/beer_trivia_q5.html');
+include('pages/beer_trivia_q5.html');
 include('pages/footer.html');
 ?>
 
